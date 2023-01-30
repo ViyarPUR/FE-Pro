cce1ae7776075abfe61b46447d6bba30b1f616f5
ViyarPUR <aleksandr0500@ukr.net>
2023-01-30 12:46:43 +0200

    leson 8

--- 8 leson/js.js
+++ 8 leson/js.js
@@ -1,3 +1,41 @@
-window.onload = () =>{
+    function submitSpisok (){
+        let list = document.getElementById('listMy');
+        let userData = document.getElementById('exampleInputEmail1');
+        let timeData = new Date().toLocaleString();
+        let userAnswer = userData.value;
+        let allData = document.createElement('div');
+        allData.className = "all"
+        allData.innerText = `User ${userAnswer}. Time create ${timeData}`;
+        list.appendChild(allData);
+
+        let allInfo = document.getElementsByClassName('all');
+        if(allInfo.length > 5){
+            list.removeChild(list.firstElementChild)
+        }
+    }
     
-}
+    // function submitSpisok (){
+    //     let list = document.querySelector('.listMy');
+    //     let userData = document.getElementById('exampleInputEmail1');
+    //     let timeData = document.createElement('div');
+    //     let textData = document.createElement('div');
+    //     textData.className = "texting";
+    //     textData.innerHTML = userData.value;
+    //     timeData.innerHTML = new Date().toLocaleString();
+    //     let allData = document.createElement('div');
+    //     allData.className = 'all';
+    //     allData.appendChild(textData);
+    //     allData.appendChild(timeData);
+    //     let elementsData = document.getElementsByClassName('all');
+
+    //     if (elementsData.length <= 4){
+    //         list.appendChild(allData);
+    //     }
+    //     else{
+    //         list.firstChild.remove();
+    //         list.appendChild(allData);
+    //     }
+
+    // }
+
+    

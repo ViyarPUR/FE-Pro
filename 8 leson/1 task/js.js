e309ae26d17520304682fcf9f87ccab017de9672
ViyarPUR <aleksandr0500@ukr.net>
2023-01-31 14:44:01 +0200

    home work 8

--- 8 leson/1 task/js.js
+++ 8 leson/1 task/js.js
@@ -1,18 +1,21 @@
+
     function submitSpisok (){
         let list = document.getElementById('listMy');
         let userData = document.getElementById('exampleInputEmail1');
         let timeData = new Date().toLocaleString();
         let userAnswer = userData.value;
         let allData = document.createElement('div');
-        allData.className = "all"
-        allData.innerText = `User ${userAnswer}. Time create ${timeData}`;
+        allData.className = "all shadow rounded-4 p-3 m-2"
+        allData.innerText = `User ${userAnswer} . Time create ${timeData}`;
         list.appendChild(allData);
-
         let allInfo = document.getElementsByClassName('all');
+
         if(allInfo.length > 5){
             list.removeChild(list.firstElementChild)
         }
     }
+
+    
     
     // function submitSpisok (){
     //     let list = document.querySelector('.listMy');
